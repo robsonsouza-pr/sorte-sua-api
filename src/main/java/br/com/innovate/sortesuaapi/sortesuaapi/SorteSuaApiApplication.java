36e231f116263bc915485987a445498072ce65bd
= <=>
2018-01-12 22:00:50 -0200

    Commit inicial

---
+++ src/main/java/br/com/innovate/sortesuaapi/sortesuaapi/SorteSuaApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.innovate.sortesuaapi.sortesuaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorteSuaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorteSuaApiApplication.class, args);
+	}
+}
