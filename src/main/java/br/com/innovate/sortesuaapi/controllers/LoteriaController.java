2e5ac214c38e81ecf1bdfd848e40fe2472959e71
= <=>
2018-01-18 21:43:44 -0200

    Cadastrando loteria

--- src/main/java/br/com/innovate/sortesuaapi/controllers/LoteriaController.java
+++ src/main/java/br/com/innovate/sortesuaapi/controllers/LoteriaController.java
@@ -3,12 +3,18 @@ package br.com.innovate.sortesuaapi.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,12 +49,43 @@ public class LoteriaController {
 		response.setData(loteriasDto);
 		return ResponseEntity.ok(response);
 	}
+	
+	@PostMapping
+	public ResponseEntity<Response<LoteriaDto>> cadastrar(@Valid @RequestBody LoteriaDto loteriaDto, BindingResult result){
+		log.info("Cadastrando uma loteria");
+		Response<LoteriaDto> response = new Response<LoteriaDto>();
+		validarLoteria(loteriaDto, result);
+		if(result.hasErrors()) {
+			log.error("Erro validando dados de cadastro de Loteria: {}", result.getAllErrors());
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+		Loteria loteria = converterParaEntidade(loteriaDto);
+		loteriaService.cadastrar(loteria);
+		response.setData(converterParaDto(loteria));
+		return ResponseEntity.ok(response);
+	}
+
+	private void validarLoteria(LoteriaDto loteriaDto, BindingResult result) {
+		Loteria loteria = loteriaService.findByNome(loteriaDto.getNome());
+		
+		if (loteria != null) {
+			result.addError(new ObjectError("Loteria", "Loteria já cadastrada."));
+		}
+		
+	}
 
 	private LoteriaDto converterParaDto(Loteria item) {
 		LoteriaDto loteriaDto = new LoteriaDto();
 		loteriaDto.setId(item.getId());
 		loteriaDto.setNome(item.getNome());
 		return loteriaDto;
+	}
+	
+	private Loteria converterParaEntidade(LoteriaDto loteriaDto) {
+		Loteria loteria = new Loteria();
+		loteria.setNome(loteriaDto.getNome());
+		return loteria;
 	}
 
 }
--- src/main/java/br/com/innovate/sortesuaapi/services/LoteriaService.java
+++ src/main/java/br/com/innovate/sortesuaapi/services/LoteriaService.java
@@ -19,4 +19,12 @@ public class LoteriaService {
 		return loteriaRepository.findAll();
 	}
 
+	public Loteria findByNome(String nome) {
+		return loteriaRepository.findByNome(nome);
+	}
+
+	public void cadastrar(Loteria loteria) {
+		loteriaRepository.save(loteria);		
+	}
+
 }
